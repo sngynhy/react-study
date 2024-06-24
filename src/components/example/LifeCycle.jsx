7bfcf7b7b6d842d17b5b2214baa4d03a2ea6b03d
sngynhy <sngynhy@gmail.com>
2024-06-24 16:34:03 +0900

    컴포넌트 활성, 비활성 > 라이프 사이클 테스트

--- src/components/example/LifeCycle.jsx
+++ src/components/example/LifeCycle.jsx
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 class Lifecycle extends React.Component {
     // 1. 마운트(Mounting)
     constructor (props) {
-        console.log('constructor')
+        console.log('constructor', props)
         // 컴포넌트 생성자 메서드 > 컴포넌트 생성 시 제일 먼저 호출
         // this.props, this.state 접근 가능, 리액트 요소 반환
         super(props)
-        this.state = { color: "red" }
+        this.state = { color: props.color }
     }
     static getDerivedStateFromProps (props, state) {
-        console.log('getDerivedStateFromProps')
+        console.log('getDerivedStateFromProps', props, state)
         // DOM에서 요소를 렌더링하기 직전 호출되는 메서드
         // props로부터 파생된 state를 가져옴
         // 즉, props로 받아온 값을 state에 할당
@@ -20,16 +20,16 @@ class Lifecycle extends React.Component {
         console.log('render')
         // 컴포넌트 렌더링 메서드 > HTML을 DOM에 출력
         return (
-            <h1>My Favorite Color is {this.state.color}</h1>
+            <h1>Selected Color is <div style={{backgroundColor: this.state.color, width: '40px', height: '25px', display: 'inline-block'}}></div></h1>
         )
     }
     componentDidMount () {
         console.log('componentDidMount')
         // 컴포넌트 렌더링 후 호출되는 메서드
         // 주로 DOM을 사용해야 하는 외부 라이브러리 연동, api 호출 등을 수행
-        setTimeout(() => {
-            this.setState({ color: "yellow" })
-        }, 1000)
+        // setTimeout(() => {
+        //     this.setState({ color: "yellow" })
+        // }, 1000)
     }
 
     // 2. 업데이트(Updating)
@@ -39,8 +39,8 @@ class Lifecycle extends React.Component {
     // }
     shouldComponentUpdate () {
         console.log('shouldComponentUpdate')
-        // 컴포넌트의 리렌더링을 계속 할지 말지 여부 지정 > boolean 반환 default : true
-        // return false
+        // 컴포넌트 리렌더링 여부 지정 > (default: true)
+        return true
     }
     // render () {
         // 업데이트하여 리렌더링
@@ -52,6 +52,7 @@ class Lifecycle extends React.Component {
         // 해당 메서드가 있는 경우 반드시 componentDidUpdate() 메서드도 있어야 함!
         console.log('업데이트 전', prevProps, prevState)
         console.log('업데이트 후', this.props, this.state)
+        return null
     }
     componentDidUpdate () {
         console.log('componentDidUpdate')
@@ -60,9 +61,9 @@ class Lifecycle extends React.Component {
 
     // 3. 언마운트(Unmounting)
     componentWillUnmount () {
-        console.log('componentWillUnmount')
+        console.log('componentWillUnmount 컴포넌트 제거!')
         // 컴포넌트가 DOM에서 제거될 시 호출
-        alert('컴포넌트 제거!')
+        // alert('컴포넌트 제거!')
     }
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,33 @@ import Lifecycle from '../components/example/LifeCycle'
 // 컴포넌트명은 무조건 대문자로!
 // 과제) setInterval을 이용해서 1초 주기로 현재 시각 업데이트 로직 구현하기!
 const Home = () => {
-    let [color, setColor] = useState('green')
+    let [colorIndex, setColorIndex] = useState(1)
+    const colorList = ['red', 'yellow', 'green', 'blue', 'white', 'black']
+    function changeColor (e) {
+        setColorIndex(e.target.value)
+    }
+
+    const [isActive, setIsActive] = useState(false);
+    function handleButtonClick () {
+        setIsActive(!isActive)
+    }
+    function lifeCycleTestComponent () {
+        return (<div>
+            <select onChange={changeColor} value={colorIndex}>
+                { colorList.map((color, i) => (<option key={i} value={i}>{color}</option>)) }
+            </select><Lifecycle color={colorList[colorIndex]} />
+        </div>)
+    }
     return (
         <div>
             <h1>🎈 Home Page 🎈</h1>
             <Clock />
-            {/* <Lifecycle color={color} /> */}
+
+            {/* <button onClick={() => {setIsActive(!isActive)}}> */}
+            <button onClick={handleButtonClick}>
+                {isActive ? 'Deactivate LifeCycle Component' : 'Activate LifeCycle Component'}
+            </button>
+            {isActive && lifeCycleTestComponent()}
         </div>
     )
 }
